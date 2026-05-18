25b5ba8076018c24a5267ac571504711667defc9
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-05-18 22:50:20 +0530

    List view

--- testProjects/app/src/main/java/com/example/testprojects/MainActivity.java
+++ testProjects/app/src/main/java/com/example/testprojects/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.testprojects;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +14,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+ListView lv;
+String fruits[] = {"Apple", "Mango", "Banana", "Grapes", "Orange","Apple", "Mango", "Banana", "Grapes", "Orange","Apple", "Mango", "Banana", "Grapes", "Orange","Apple", "Mango", "Banana", "Grapes", "Orange","Apple", "Mango", "Banana", "Grapes", "Orange"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        lv = findViewById(R.id.listview1);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, fruits);
+
+        lv.setAdapter(adapter);
+//        lv.setOnItemClickListener()
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view,
+                                    int position,
+                                    long id) {
+
+                Toast.makeText(MainActivity.this,
+                        fruits[position],
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
