80d1f04bbd6fff09f097a0de5173b13ee0babbc9
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-05-18 23:09:17 +0530

    TABLE lAYOUT

--- testProjects/app/src/main/java/com/example/testprojects/MainActivity.java
+++ testProjects/app/src/main/java/com/example/testprojects/MainActivity.java
@@ -14,32 +14,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-ListView lv;
-String fruits[] = {"Apple", "Mango", "Banana", "Grapes", "Orange","Apple", "Mango", "Banana", "Grapes", "Orange","Apple", "Mango", "Banana", "Grapes", "Orange","Apple", "Mango", "Banana", "Grapes", "Orange","Apple", "Mango", "Banana", "Grapes", "Orange"};
-    @Override
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        lv = findViewById(R.id.listview1);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, fruits);
 
-        lv.setAdapter(adapter);
-//        lv.setOnItemClickListener()
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent,
-                                    View view,
-                                    int position,
-                                    long id) {
 
-                Toast.makeText(MainActivity.this,
-                        fruits[position],
-                        Toast.LENGTH_SHORT).show();
-
-            }
-        });
     }
 }
