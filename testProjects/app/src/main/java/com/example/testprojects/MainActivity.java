d36733f13335c64201ce1a04d6d148d2524236d7
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-05-19 11:04:59 +0530

    TABLE lAYOUT

--- testProjects/app/src/main/java/com/example/testprojects/MainActivity.java
+++ testProjects/app/src/main/java/com/example/testprojects/MainActivity.java
@@ -2,25 +2,31 @@ package com.example.testprojects;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-@Override
+
+    Button b;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        b = findViewById(R.id.button1);
 
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Toast.makeText(MainActivity.this,
+                        "Submitted",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
