f37e8d467d7a3194f955333f8d49ebbad53bde1c
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-05-19 12:25:29 +0530

    Implicit intent

--- ImplicitIntentExample/app/src/main/java/com/example/implicitintentexample/MainActivity.java
+++ ImplicitIntentExample/app/src/main/java/com/example/implicitintentexample/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.implicitintentexample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        b= findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+             @Override
+            public void onClick(View v) {
+                 Toast.makeText(MainActivity.this,"Opening Browser......",Toast.LENGTH_SHORT).show();
+                 Intent i = new Intent(Intent.ACTION_DIAL);
+                 i.setData(Uri.parse("tel:9356121442"));
+                 startActivity(i);
+             }
         });
     }
 }
